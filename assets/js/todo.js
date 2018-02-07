e1f4064f916c790a63751a07b899fa25915946e5
sym28 <29101705+sym28@users.noreply.github.com>
2018-02-07 17:40:28 +0000

    added function to add and delete todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,20 @@
-alert('connected');
+// Check off todos by clicking
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass('completed');
+});
+
+// Click X to delete li
+$('ul').on('click','span',function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event){
+    if (event.which === 13) {
+       var newTodo = $(this).val()
+       $(this).val('');
+       $('ul').append('<li><span>X</span> ' + newTodo + '</li>')
+    }
+})
