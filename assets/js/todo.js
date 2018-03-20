7d6b263e7ec5f5cd9f716bac4bf03828b1e5c7b9
sym28 <29101705+sym28@users.noreply.github.com>
2018-03-20 16:54:11 +0000

    added toggle function for input

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -15,7 +15,10 @@ $('input[type="text"]').keypress(function(event){
     if (event.which === 13) {   //enter-key press
        var newTodo = $(this).val()
        $(this).val('');
-       $('ul').append('<li><span><i class="fas fa-trash"></i></span> ' + newTodo + '</li>') 
-       
+       $('ul').append('<li><span><i class="fas fa-trash"></i></span> ' + newTodo + '</li>')      
     }
-}) 
+});
+
+$('.fa-plus').click(function(){
+    $('input[type="text"]').fadeToggle();
+});
